30aae5a582fa7767bec10a02970436f68523d6a3
sunilmeena26 <sunilmeena641085@gmail.com>
2025-03-04 18:16:30 +0530

    [Sunil Meena] Add EmployeePayrollRepository interface extending JpaRepository for basic CRUD operations on Employee entity.

--- src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -4,7 +4,7 @@ import com.bridgelabz.employeepayrollapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//Create an interface EmployeePayrollRepository to extends JpaRepository which provides built-in methods for basic database CRUD operations
+//Create an interface EmployeePayrollRepository to extends JpaRepository which provides built-in methods for perform basic CRUD operations on database
 @Repository
 public interface EmployeePayrollRepository extends JpaRepository<Employee,Long> {
 }
