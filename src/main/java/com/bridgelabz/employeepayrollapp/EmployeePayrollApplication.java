52388a96255a8c99c4b0c6d37a9ae115c8d88d37
sunilmeena26 <sunilmeena641085@gmail.com>
2025-03-04 18:14:05 +0530

    [Sunil Meena] Add EmployeePayrollApplication main class code file

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main class
 @SpringBootApplication
 public class EmployeePayrollApplication {
 
